5e8b829d2855ee8a3693246c0ca304734909dd17
kamaleshKDK <kamaleshkdk@gmail.com>
2022-01-09 14:45:32 +0530

    First commit NodeExpress

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+let date = new Date();
+let time = (date.getDate()) + "-" + (date.getMonth()) + "-" + (date.getFullYear()) + "-" + (date.getHours()) +"-"+(date.getMinutes())+"-"+(date.getSeconds())
+let file = time + ".txt";
+let timeStamp = date.getTime().toString();
+
+
+let options = {
+    origin: "*"
+}
+app.use(cors(options))
+
+app.get("/time", function (req, res) {
+    res.json(
+
+        fs.writeFile(file, timeStamp, function (err) {
+            if (err) throw err;
+            console.log("File Created !");
+        })
+    )
+})
+
+
+
+app.listen(3000);
